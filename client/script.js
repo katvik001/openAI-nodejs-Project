072e5a7705bbc6b2f8ff79bf58b266948b092429
katvik001 <katvik001@gmail.com>
2023-02-06 16:08:12 +0900

    updated client server details

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://vikash-ai.jtcs.in:8080', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
